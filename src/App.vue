bfc1be8c7e5ce2d18e3131f8416894bee5e20e67
糖小米 <tangxiaomiemail@gmail.com>
2019-09-11 10:21:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <div class="warp">
+            hello
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+declare module 'vue/types/vue' {
+    interface Vue {
+        [key: string]: any,
+    }
+}
+@Component({})
+export default class App extends Vue {
+    
+}
+</script>
+<style lang="stylus">
+html, body
+    width 100%
+    height 100%
+</style>
