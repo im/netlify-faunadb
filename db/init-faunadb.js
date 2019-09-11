fb07aab1d58adbe5666ab90b687c1680f170838e
糖小米 <tangxiaomiemail@gmail.com>
2019-09-11 21:26:30 +0800

    add faunadb

---
+++ db/init-faunadb.js
@@ -0,0 +1,22 @@
+const faunadb = require('faunadb')
+const chalk = require('chalk')
+const q = faunadb.query
+
+
+if (!process.env.FAUNADB_SECRET) {
+    console.log(chalk.yellow('Required FAUNADB_SERVER_SECRET enviroment variable not found.'))
+}
+
+function initFaunaDB () {
+    const client = new faunadb.Client({
+        secret: 'fnADYBQA19ACD82MoNTqoTOTxFZUvFjDU3IQh2Lr'
+    })
+
+    client.query(q.CreateCollection({ name: 'posts' }))
+
+    console.log(chalk.cyan('Your collections have been initilized successfully'))
+}
+
+initFaunaDB()
+
+
