bcf43c406e8c30d002f2f7b1c146b1331b16c75f
糖小米 <tangxiaomiemail@gmail.com>
2019-09-12 07:49:16 +0800

    update

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,11 +1,8 @@
-import client, { q } from './faunadb'
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: {
-            token: process.env.FAUNADB_SECRET,
-            client: client,
-            q: q
+            token: process.env.FAUNADB_SECRET
         }
     })
 }
