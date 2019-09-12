907714b63eeab5dc5c9b7f503a4a3da3f4199314
糖小米 <tangxiaomiemail@gmail.com>
2019-09-12 15:40:48 +0800

    udpate

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 var webpack = require('webpack');
 
-/* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
-// https://danbruder.com/blog/typeerror-require-is-not-a-function-webpack-faunadb/
 module.exports = {
   mode: 'development',
   plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
